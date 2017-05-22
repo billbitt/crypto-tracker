a535eca4d7d4202d45cd14e5a4e5f2f735bf8a7e
bill bittner <bittner.w@gmail.com>
2017-05-22 15:20:18 -0700

    crypto-tracker

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # crypto-tracker
 a node application that you can use to track the status of your crypto portfolio
+
+## how to use
+* clone this repo
+* run npm install from inside the project's root folder
+* update the `portfolio.js` file in the root directory to reflect your portfolio details
+* start your mysql server
+* run `node server`
+* visit http://localhost:3000
+* enjoy
+
+
+## future dev
+* set up a server on AWS to gather price info and put in a sql database
+	* set up a smart contract that people can pay into and then it will pay out to aws to support the server for everyone who wants to run this app client side
+	* or just refactor so all the http calls are made client side and responses are stored in an object client-side in local storage on the browser, cutting out the need for a back end alltogether.
+* create an option for users to store their portfolio data so they don't have to re-enter their data each time 
+	* in short term could store this in local storage in the browser, but would like to store this more permanently like in a db on the server
+* additional module: you put in your public addresses, and the program crawls those wallets via blockchain explorers to get your current holdings for you.  i.e. you put in your wallet addresses and it does the rest.
+* additional module: historical data on your portfolio. e.g. show a graph of your portfolio over time.
+
--- workers/updatePrices.js
+++ workers/updatePrices.js
@@ -3,17 +3,17 @@ var db = require("../models");
 
 // export functionality
 module.exports = function(updateSpeed){
-
-	// every 1 seconds, get the new values and update the current Values object
+	/* BTC */
+	// define a function to get the btc price update 
 	function updateBtcPrice(){
-
+		// define the options for the http request 
 		var options = {
 			url: 'https://api.coinbase.com/v2/prices/BTC-USD/spot',
 			headers: {
 				'CB-VERSION': '2017-05-20'
 			}
 		};
-
+		// define the callback for the request
 		function callback(error, response, body){
 			if (error){
 				console.log({ result: 'error', error: error });
@@ -33,22 +33,23 @@ module.exports = function(updateSpeed){
 				return;
 			});
 		}
-
+		// make the request 
 		request.get(options, callback);
 	}
-
+	// set a 'worker' to make the request every 'updateSpeed' seconds
 	setInterval(updateBtcPrice, updateSpeed * 1000);
-
-	// every 1 seconds, get the new values and update the current Values object
+	
+	/* ETH */
+	// define a function to get the eth price update 
 	function updateEthPrice(){
-
+		// define the http request options 
 		var options = {
 			url: 'https://api.coinbase.com/v2/prices/ETH-USD/spot',
 			headers: {
 				'CB-VERSION': '2017-05-20'
 			}
 		};
-
+		// define the callback function for the request 
 		function callback(error, response, body){
 			if (error){
 				console.log({ result: 'error', error: error });
@@ -67,10 +68,45 @@ module.exports = function(updateSpeed){
 				return;
 			});
 		}
-
+		// make the request
 		request.get(options, callback);
 	}
-
+	// set a 'worker' to make the call every so many seconds 
+	setInterval(updateEthPrice, updateSpeed * 1000);
+	
+	/* ETH */
+	// define a function to get the eth price update 
+	function updateEthPrice(){
+		// define the http request options 
+		var options = {
+			url: 'https://api.coinbase.com/v2/prices/ETH-USD/spot',
+			headers: {
+				'CB-VERSION': '2017-05-20'
+			}
+		};
+		// define the callback function for the request 
+		function callback(error, response, body){
+			if (error){
+				console.log({ result: 'error', error: error });
+				return;
+			};
+			//console.log({result: 'response received', statusCode: response.statusCode, body: body});
+			//console.log(JSON.parse(body).data.amount)
+			db.CryptoValue.update({
+				value: JSON.parse(body).data.amount,
+			}, {
+				where: {
+					name: "eth"
+				}
+			}).then(function(data){
+				console.log("eth update complete");
+				return;
+			});
+		}
+		// make the request
+		request.get(options, callback);
+	}
+	// set a 'worker' to make the call every so many seconds 
 	setInterval(updateEthPrice, updateSpeed * 1000);
 
 }
